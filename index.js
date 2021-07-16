bb41004cb15564733276ea42cc01be29b48db6be
mpyw <ryosuke_i_628@yahoo.co.jp>
2021-07-16 17:57:59 +0900

    feat: 🎸 Support script-tag importing

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-module.exports = function noerr(callback) {
-  try {
-    return callback.apply(null, Array.prototype.slice.call(arguments, 1));
-  } catch (e) {
-    if (e.constructor !== TypeError) {
-      throw e;
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.NoErr = {}));
+}(this, (function (exports) { 'use strict';
+
+  exports.default = function noerr(callback) {
+    try {
+      return callback.apply(null, Array.prototype.slice.call(arguments, 1));
+    } catch (e) {
+      if (e.constructor !== TypeError) {
+        throw e;
+      }
     }
-  }
-};
+  };
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+})));
